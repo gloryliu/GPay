9f80d0542b509af6bfab8b0a365e145ee29d839f
liuzhenrong <liu.zhenrong@jyall.com>
2017-12-06 16:24:17 +0800

    搭建支付系统

---
+++ gpay/src/main/java/com/liu/glory/gpay/GpayApplication.java
@@ -0,0 +1,12 @@
+package com.liu.glory.gpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpayApplication.class, args);
+	}
+}
